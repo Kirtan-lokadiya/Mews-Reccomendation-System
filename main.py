c75b189a0d551da932c6ea96d4beab1f90384bb3
swarajbari18 <007swarajbari@gmail.com>
2023-10-25 13:45:31 +0530

    test API

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from News_Reccomendation_System import logger
 # mentioning src was not necesarry because we pip installed the project_porfolio_one as a library when we wrote our setup.py and ran -e . 
 # and ran -e . in our requirements.txt
 
-logger.info("Welcome to   SWARAJ BARI' s  Custom End-to-End Project")
+logger.info("Welcome to   SWARAJ BARI' s  News Reccomendation System Project")
 
 
 
 from News_Reccomendation_System.pipeline.step1_data_ingestion import run_data_ingestion
 from News_Reccomendation_System.pipeline.step2_data_transformation import run_data_transformation
-from portfolio_project_one.pipeline.step3_model_training import run_model_trainer
+from News_Reccomendation_System.pipeline.step3_model_training import run_model_trainer
 # from portfolio_project_one.pipeline.step5_model_evaluation import run_model_evaluator
 
 
